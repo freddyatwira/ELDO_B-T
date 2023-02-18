c76fc113b01b87c9b79c3ba9e36821ce64970798
freddywaigwa <freddywaigwa@gmail.com>
2023-02-18 22:03:13 +0300

    added calculator

---
+++ script.js
@@ -0,0 +1,22 @@
+let distance;
+
+btn1 = document.getElementById('btn1');
+
+btn2 = document.getElementById('btn2');
+
+messageEl = document.getElementById('message-el')
+
+function openEditor(){
+    distance = parseInt(prompt('Enter distance in km'));
+    messageEl.textContent = `Calculate the cost of ${distance}`
+    btn1.textContent = 'ENTER NEW'
+}
+
+function calCost(){
+    if( distance <= 20){
+        messageEl.textContent=`The cost of ${distance} km is ${3500 + distance * 150}`
+    }
+    else{
+        messageEl.textContent=`Contact us for offers`
+    }
+}
